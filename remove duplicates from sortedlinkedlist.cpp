fc874cf655864891deafe65eda7307c0a9dde9ef
Asaad Naji <assaadnaji014@gmail.com>
2024-11-11 14:34:22 +0100

    added remove duplicates from sorted linked list

---
+++ remove duplicates from sortedlinkedlist.cpp
@@ -0,0 +1,37 @@
+#include <set>
+#include <iostream>
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head)
+            return NULL;
+        std::set<int> mySet;
+        while(head)
+        {
+            mySet.insert(head->val);
+            head = head->next;
+        }
+        ListNode *res = NULL;
+        ListNode *tmp = NULL;
+        for(std::set<int>::iterator it = mySet.begin(); it != mySet.end(); ++it){
+            if(it == mySet.begin()){
+                res = new ListNode(*it, nullptr);
+                tmp = res;
+            }else{
+                tmp->next= new ListNode(*it, nullptr);
+                tmp = tmp->next;
+            }
+        }
+        return res;
+    }
+};
+
